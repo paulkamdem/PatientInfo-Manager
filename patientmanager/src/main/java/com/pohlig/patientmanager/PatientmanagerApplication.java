22a19a3a7a3c895a89e4852e1f2e012c5a2e3878
Allexandra Sorelle Ngoungoure Mfouapon <paul_kamdem@outlook.com>
2025-08-11 14:24:10 +0200

    start backend with spring boot

---
+++ patientmanager/src/main/java/com/pohlig/patientmanager/PatientmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.pohlig.patientmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientmanagerApplication.class, args);
+	}
+
+}
